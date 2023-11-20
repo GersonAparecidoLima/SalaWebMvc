dbffd50c6c29d89ea327553c80ea3e0c228721a1
Gerson Aparecido Lima <gerson.lima@yduqs.com.br>
2023-11-20 10:03:47 -0300

    SellersController

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -9,15 +9,39 @@ namespace SalesWebMvc.Data
 {
     public class SalesWebMvcContext : DbContext
     {
+        // O objetico do DbContext e mapea as clasee para que ela 
+        // se torne banco de dados
+        // E o DbSet ser para mapea a classe 
+
+        //DefaultConnection vai ser o  name="DefaultConnection" no  ConnectionStrings 
         public SalesWebMvcContext (DbContextOptions<SalesWebMvcContext> options)
             : base(options)
         {
         }
 
+        //Mapeando a classe
         public DbSet<Department> Department { get; set; }
                
         public DbSet<Seller> Seller { get; set; }
         public DbSet<SalesRecord> SalesRecord { get; set; }
+
+
+        //******************************************
+        //para o Entity não aplicar o plural
+        //******************************************
+        //Exemplo
+        //public DbSet<Aluno> Alunos { get; set; }
+
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+            //******************************************
+            //para o Entity não aplicar o plural
+            //******************************************
+
+            //modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            //modelBuilder.Entity<Aluno>().ToTable("Alunos");
+            //base.OnModelCreating(modelBuilder);
+        //}
 
     }
 }
